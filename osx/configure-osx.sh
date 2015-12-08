20aeb72ffe5f17c5e6ca1c8e0dae65b44f765bdb
Alex Metzger <asm@asm.io>
2015-12-08 17:09:13 -0500

    Rewrite configure-osx.sh to something I actually use

--- osx/configure-osx.sh
+++ osx/configure-osx.sh
@@ -1,142 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# Taken from https://gist.github.com/2260182
-# Modified to suit my exacting taste
+# assumes XCode command line stuff already installed
 
-echo "Enable subpixel font rendering on non-Apple LCDs"
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
+echo "setup key repeat"
+defaults write NSGlobalDomain KeyRepeat -int 0.1
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
-echo "Make Dock icons of hidden applications translucent"
-defaults write com.apple.dock showhidden -bool true
-
-echo "Disable menu bar transparency"
-defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
-
-echo "Show remaining battery time; hide percentage"
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-defaults write com.apple.menuextra.battery ShowTime -string "NO"
-
-echo "Allow quitting Finder via ⌘ + Q; doing so will also hide desktop icons"
-defaults write com.apple.finder QuitMenuItem -bool true
-
-echo "Use current directory as default search scope in Finder"
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-
-echo "Show Path bar in Finder"
-defaults write com.apple.finder ShowPathbar -bool true
-
-echo "Show Status bar in Finder"
-defaults write com.apple.finder ShowStatusBar -bool true
-
-echo "Expand save panel by default"
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-
-echo "Expand print panel by default"
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-
-echo "Disable the “Are you sure you want to open this application?” dialog"
-defaults write com.apple.LaunchServices LSQuarantine -bool false
-
-echo "Disable shadow in screenshots"
-defaults write com.apple.screencapture disable-shadow -bool true
-
-echo "Enable spring loading for all Dock items"
-defaults write enable-spring-load-actions-on-all-items -bool true
-
-echo "Show indicator lights for open applications in the Dock"
-defaults write com.apple.dock show-process-indicators -bool true
-
-echo "Disable press-and-hold for keys in favor of key repeat"
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-echo "Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 0.02
-
-echo "Set a shorter Delay until key repeat"
-defaults write NSGlobalDomain InitialKeyRepeat -int 12
-
-echo "Disable auto-correct"
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-
-echo "Enable AirDrop over Ethernet and on unsupported Macs running Lion"
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
-
-echo "Disable disk image verification"
-defaults write com.apple.frameworks.diskimages skip-verify -bool true
-defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
-defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
-
-echo "Increase window resize speed for Cocoa applications"
-defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
-
-echo "Avoid creating .DS_Store files on network volumes"
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-
-echo "Disable the warning when changing a file extension"
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-echo "Show item info below desktop icons"
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
-
-echo "Enable snap-to-grid for desktop icons"
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy name" ~/Library/Preferences/com.apple.finder.plist
-
-echo "Disable the warning before emptying the Trash"
-defaults write com.apple.finder WarnOnEmptyTrash -bool false
-
-echo "Require password 1 minute after sleep or screen saver begins"
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 60
-
-echo "Disable Safari’s thumbnail cache for History and Top Sites"
-defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
-
-echo "Enable Safari’s debug menu"
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-
-echo "Make Safari’s search banners default to Contains instead of Starts With"
-defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
-
-echo "Add a context menu item for showing the Web Inspector in web views"
+echo "setup system params"
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-echo "Only use UTF-8 in Terminal.app"
-defaults write com.apple.terminal StringEncodings -array 4
-
-echo "Disable the Ping sidebar in iTunes"
-defaults write com.apple.iTunes disablePingSidebar -bool true
-
-echo "Disable all the other Ping stuff in iTunes"
-defaults write com.apple.iTunes disablePing -bool true
-
-echo "Make ⌘ + F focus the search input in iTunes"
-defaults write com.apple.iTunes NSUserKeyEquivalents -dict-add "Target Search Field" "@F"
-
-echo "Disable the “reopen windows when logging back in” option"
-defaults write com.apple.loginwindow TALLogoutSavesState -bool false
-defaults write com.apple.loginwindow LoginwindowLaunchesRelaunchApps -bool false
-
-echo "Disable Resume system-wide"
-defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
-
-echo "Reset Launchpad"
-[ -e ~/Library/Application\ Support/Dock/*.db ] && rm ~/Library/Application\ Support/Dock/*.db
-
-echo "Show the ~/Library folder"
 chflags nohidden ~/Library
+mkdir ~/Desktop/Screenshots
+defaults write com.apple.screencapture location ~/Desktop/Screenshots
 
-echo "Disable local Time Machine backups"
-hash tmutil &> /dev/null && sudo tmutil disablelocal
-
-echo "Remove Dropbox’s green checkmark icons in Finder"
-file=/Applications/Dropbox.app/Contents/Resources/check.icns
-[ -e "$file" ] && mv -f "$file" "$file.bak"
-unset file
-
-echo "Disable natural scrolling"
-defaults write ~/Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false
-
-echo "Kill affected applications"
-for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
-
-echo "!!!! LOG OUT TO COMPLETE CONFIGURATION! !!!!"
+echo "install brew"
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
