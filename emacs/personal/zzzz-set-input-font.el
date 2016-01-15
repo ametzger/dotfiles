6e42626c5206a0910f34e15e30739b835bd2ba47
Alex Metzger <asm@asm.io>
2016-01-15 18:27:42 -0500

    actually default to input mono font in emacs

---
+++ emacs/personal/zzzz-set-input-font.el
@@ -0,0 +1,3 @@
+;(set-frame-font "Input Mono Regular-13")
+(set-default-font "-*-Input Mono-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1")
+;(set-face-attribute 'default nil :font "Input Mono Regular-12" :height 100)
