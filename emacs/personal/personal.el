6cb4b67f20317ec887b50c9efad8eae4575fb220
Alex Metzger <alex.metzger@gmail.com>
2016-10-31 21:21:22 -0400

    Simplify frame titles, fix single semicolon comments

--- emacs/personal/personal.el
+++ emacs/personal/personal.el
@@ -2,28 +2,28 @@
 ;;; for use with bbatsov's Prelude
 ;;; https://github.com/bbatsov/prelude
 
-; disable auto save
+;; disable auto save
 (setq auto-save-default nil)
-; disable guru (warnings when arrow keys are used)
+;; disable guru (warnings when arrow keys are used)
 (setq prelude-guru nil)
-; disable emacs lisp linting
+;; disable emacs lisp linting
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
-(setq prelude-flyspell nil)
+;(setq prelude-flyspell nil)
 
-; initialize packages
+;; initialize packages
 (prelude-require-package 'solarized-theme)
 (prelude-require-package 'csharp-mode)
 (prelude-require-package 'hlinum)
 (prelude-require-package 'ag)
 
-; custom file extension mappings
+;; custom file extension mappings
 (add-to-list 'auto-mode-alist '("\\.cake\\'" . csharp-mode))
 (add-to-list 'auto-mode-alist '("\\.fish\\'" . shell-script-mode))
 
-; add line numbers
-; based on https://www.emacswiki.org/emacs/LineNumbers
+;; add line numbers
+;; based on https://www.emacswiki.org/emacs/LineNumbers
 
-; click on a line to select
+;; click on a line to select
 (eval-after-load 'linum
   '(progn
      (defvar *linum-mdown-line* nil)
@@ -36,8 +36,8 @@
            (goto-char (window-start))
            (next-line (1- click-y))
            (setq line-move-visual line-move-visual-store)
-           ;; If you are using tabbar substitute the next line with
-           ;; (line-number-at-pos))))
+           ;;; If you are using tabbar substitute the next line with
+           ;;; (line-number-at-pos))))
            (1+ (line-number-at-pos)))))
 
      (defun md-select-linum ()
@@ -51,7 +51,7 @@
        (interactive)
        (when *linum-mdown-line*
          (let (mu-line)
-           ;; (goto-line (line-at-click))
+           ;;; (goto-line (line-at-click))
            (setq mu-line (line-at-click))
            (goto-line (max *linum-mdown-line* mu-line))
            (set-mark (line-end-position))
@@ -63,19 +63,19 @@
      (global-set-key (kbd "<left-margin> <mouse-1>") 'mu-select-linum)
      (global-set-key (kbd "<left-margin> <drag-mouse-1>") 'mu-select-linum)))
 
-; include a line between the numbers and the buffer
+;; include a line between the numbers and the buffer
 (setq linum-format "%4d \u2502")
 (global-linum-mode t)
 (hlinum-activate)
 
-; remap super => control on mac
+;; remap super => control on mac
 (if (eq system-type 'darwin)
     (setq mac-command-modifier 'control))
 
-; when org-mode starts, expand all nodes
+;; when org-mode starts, expand all nodes
 (setq org-startup-folded nil)
 
-; configure Input Mono font with fallbacks
+;; configure Input Mono font with fallbacks
 (cond
  ((find-font (font-spec :name "Input"))
   (set-frame-font "Input-12"))
@@ -86,7 +86,7 @@
  ((find-font (font-spec :name "Consolas"))
   (set-frame-font "Consolas-12")))
 
-; C-c C-k to pull current line to kill ring
+;; C-c C-k to pull current line to kill ring
 (defun copy-line (&optional arg)
   "Do a kill-line but copy rather than kill.  This function directly calls
     kill-line, so see documentation of kill-line for how to use it including prefix
@@ -98,3 +98,10 @@
     (kill-line arg)))
 
 (global-set-key (kbd "\C-c\C-k") 'copy-line)
+
+;; simple title
+(setq frame-title-format '(""
+                           "emacs - "
+                           (:eval (if (buffer-file-name)
+                                      (abbreviate-file-name (buffer-file-name))
+                                    "%b"))))
