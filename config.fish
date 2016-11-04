541e4c9321799f5576b27cfc7b530f1e7c5c7658
Alex Metzger <alex.metzger@gmail.com>
2016-11-03 23:24:01 -0400

    add fish PATH config

--- config.fish
+++ config.fish
@@ -1,1 +1,11 @@
+# suppress fish prompt greeting
 set -g -x fish_greeting ''
+
+# add homebrew sbin to PATH
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+# add ~/bin to PATH if it exists
+if test -d ~/bin
+    echo "Adding ~/bin to PATH"
+    set -g fish_user_paths "~/bin" $fish_user_paths
+end
