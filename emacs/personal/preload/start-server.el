b7038e61c2bbc40a3ec6c856a9dcf8347f5f1688
Alex Metzger <alex.metzger@gmail.com>
2016-11-03 22:31:38 -0400

    only disable server-ensure-safe-dir on windows

--- emacs/personal/preload/start-server.el
+++ emacs/personal/preload/start-server.el
@@ -1,8 +1,9 @@
 ;; suppress warning about unsafe directory when running
 ;; as windows admin -- I LIVE LIFE ON THE EDGE
-(require 'server)
-(when (and (>= emacs-major-version 23)
-           (equal window-system 'w32))
-  (defun server-ensure-safe-dir (dir) "Noop" t))
+(when (eq system-type 'windows-nt)
+  (require 'server)
+  (when (and (>= emacs-major-version 23)
+             (equal window-system 'w32))
+    (defun server-ensure-safe-dir (dir) "Noop" t)))
 
 (server-start)
