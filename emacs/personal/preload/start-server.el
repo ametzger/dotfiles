bc3b60a37e18870e42d55f71e8fe090ceabb60aa
Alex Metzger <alex.metzger@gmail.com>
2016-11-03 21:30:15 -0400

    EMACS DANGERZONE MOTHERFUCKERS

--- emacs/personal/preload/start-server.el
+++ emacs/personal/preload/start-server.el
@@ -1,1 +1,8 @@
+;; suppress warning about unsafe directory when running
+;; as windows admin -- I LIVE LIFE ON THE EDGE
+(require 'server)
+(when (and (>= emacs-major-version 23)
+           (equal window-system 'w32))
+  (defun server-ensure-safe-dir (dir) "Noop" t))
+
 (server-start)
