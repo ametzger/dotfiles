1b89addd424de033aa5a174145d41d4e3ac2b892
Alex Metzger <alex.metzger@gmail.com>
2016-11-04 12:49:20 -0400

    Add editor aliases to fish config, custom.el updates

--- config.fish
+++ config.fish
@@ -6,6 +6,11 @@ set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
 # add ~/bin to PATH if it exists
 if test -d ~/bin
-    echo "Adding ~/bin to PATH"
+    #echo "Adding ~/bin to PATH"
     set -g fish_user_paths "~/bin" $fish_user_paths
 end
+
+alias e "emacsclient -nac"
+alias mvim "emacsclient -nac"
+alias subl "emacsclient -nac"
+alias mate "emacsclient -nac"
