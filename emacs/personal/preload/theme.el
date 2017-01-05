368d0dbb704ebc12d3d53c921cb93822d52cd2ed
Alex Metzger <alex.metzger@gmail.com>
2017-01-04 21:40:43 -0500

    first run emacs theme strangeness.

--- emacs/personal/preload/theme.el
+++ emacs/personal/preload/theme.el
@@ -1,1 +1,8 @@
+(disable-theme 'zenburn)
+
 (setq prelude-theme 'solarized-dark)
+
+;; seems like solarized isn't loaded pre-prelude. Should only be a problem at first install.
+
+;;(if (member 'solarized-dark (custom-available-themes))
+;;    (setq prelude-theme 'solarized-dark))
