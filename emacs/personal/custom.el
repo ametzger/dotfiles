f258165c1fe406e3a8de93472fd152a66e17c70b
Alex Metzger <alex.metzger@gmail.com>
2017-01-04 22:17:47 -0500

    add gitconfig to emacs cfg

--- emacs/personal/custom.el
+++ emacs/personal/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (visual-regexp visual-regexp-steroids hlinum json-mode fish-mode haskell-mode ag racket-mode csharp-mode zop-to-char zenburn-theme volatile-highlights vkill undo-tree solarized-theme smartrep smartparens smart-mode-line projectile ov operate-on-number move-text magit guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell easy-kill discover-my-major diminish diff-hl browse-kill-ring beacon anzu ace-window))))
+    (gitconfig visual-regexp visual-regexp-steroids hlinum json-mode fish-mode haskell-mode ag racket-mode csharp-mode zop-to-char zenburn-theme volatile-highlights vkill undo-tree solarized-theme smartrep smartparens smart-mode-line projectile ov operate-on-number move-text magit guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell easy-kill discover-my-major diminish diff-hl browse-kill-ring beacon anzu ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
