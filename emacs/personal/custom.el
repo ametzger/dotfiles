dc887ee781ee61c07dc3b65aa7ec2c00d87a4e22
Alex Metzger <alex.metzger@gmail.com>
2017-01-25 21:51:24 -0500

    emacs :shit:

--- emacs/personal/custom.el
+++ emacs/personal/custom.el
@@ -3,9 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(omnisharp--curl-executable-path "/usr/bin/curl")
+ '(omnisharp-server-executable-path
+   "/Users/asm/proj/vendor/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe")
  '(package-selected-packages
    (quote
-    (gitconfig visual-regexp visual-regexp-steroids hlinum json-mode fish-mode haskell-mode ag racket-mode csharp-mode zop-to-char zenburn-theme volatile-highlights vkill undo-tree solarized-theme smartrep smartparens smart-mode-line projectile ov operate-on-number move-text magit guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell easy-kill discover-my-major diminish diff-hl browse-kill-ring beacon anzu ace-window))))
+    (csv-mode company gitconfig visual-regexp visual-regexp-steroids hlinum json-mode fish-mode haskell-mode ag racket-mode csharp-mode zop-to-char zenburn-theme volatile-highlights vkill undo-tree solarized-theme smartrep smartparens smart-mode-line projectile ov operate-on-number move-text magit guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell easy-kill discover-my-major diminish diff-hl browse-kill-ring beacon anzu ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
