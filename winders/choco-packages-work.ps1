172c23d4a1bc4c0a3cdc565ff60951f2c7b33a29
Alex Metzger <asm@asm.io>
2017-03-02 13:26:43 -0500

    choco script changes

--- winders/choco-packages-work.ps1
+++ winders/choco-packages-work.ps1
@@ -1,15 +1,14 @@
+# enable global confirm in choco
+choco feature enable -n=allowGlobalConfirmation
+
+
 choco install -y 7zip.install
 choco install -y ag
-choco install -y autohotkey.portable
-choco install -y autoit.commandline
 choco install -y awscli
 choco install -y AWSTools.Powershell
-choco install -y chocolatey
-choco install -y chocolatey-core.extension
 choco install -y clink
 choco install -y ConEmu
 choco install -y curl
-choco install -y DotNet4.5.2
 choco install -y dropbox
 choco install -y Emacs
 choco install -y fiddler
@@ -19,25 +18,22 @@ choco install -y git.install -params '"/GitAndUnixToolsOnPath"'
 choco install -y GoogleChrome
 choco install -y hackfont
 choco install -y html-tidy
-choco install -y nmap
+#choco install -y nmap
 choco install -y nodejs.install
 choco install -y notepadplusplus.install
 choco install -y P4Merge
 choco install -y pidgin
 #choco install -y pip
-choco install -y PowerShell
+choco install -y kitty
 choco install -y putty
 choco install -y putty.portable
-#choco install -y python
-#choco install -y python3
 choco install -y rdcman
 choco install -y redis-64
 choco install -y ruby
 choco install -y screenhero
 choco install -y sharpkeys
 choco install -y slack
-#choco install -y sqlyog
 choco install -y sysinternals
 choco install -y terminals
-#choco install -y UrlRewrite
+#choco install -y UrlRewrite # installed later on by install-development-iis.ps1 in backend-scripts, fails if IIS not installed
 choco install -y vlc
