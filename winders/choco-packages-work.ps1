0b5e3cdf82d77b9b2f3e815b33a4734d4b5b82cc
Alex Metzger <asm@asm.io>
2017-03-02 13:33:55 -0500

    rework choco script

--- winders/choco-packages-work.ps1
+++ winders/choco-packages-work.ps1
@@ -1,39 +1,45 @@
 # enable global confirm in choco
 choco feature enable -n=allowGlobalConfirmation
 
+$packages = @(
+    "7zip.install"
+    "ag"
+    "awscli"
+    "AWSTools.Powershell"
+    "clink"
+    "ConEmu"
+    "curl"
+    "dropbox"
+    "Emacs"
+    "fiddler"
+    "fiddler4"
+    "Firefox"
+    "GoogleChrome"
+    "hackfont"
+    "html-tidy"
+    "nodejs.install"
+    "notepadplusplus.install"
+    "P4Merge"
+    "pidgin"
+    "kitty"
+    "putty"
+    "putty.portable"
+    "rdcman"
+    "redis-64"
+    "ruby"
+    "screenhero"
+    "slack"
+    "sysinternals"
+    "terminals"
+    "vlc"
+)
 
-choco install -y 7zip.install
-choco install -y ag
-choco install -y awscli
-choco install -y AWSTools.Powershell
-choco install -y clink
-choco install -y ConEmu
-choco install -y curl
-choco install -y dropbox
-choco install -y Emacs
-choco install -y fiddler
-choco install -y fiddler4
-choco install -y Firefox
+ForEach ($packageName In $packages)
+{
+    Write-Host "Installing $packageName..."
+    choco install -y $packageName
+    Write-Host "Finished installing $packageName"
+}
+
+# Install git.install with the unix stuff (ls, etc) on %PATH%
 choco install -y git.install -params '"/GitAndUnixToolsOnPath"'
-choco install -y GoogleChrome
-choco install -y hackfont
-choco install -y html-tidy
-#choco install -y nmap
-choco install -y nodejs.install
-choco install -y notepadplusplus.install
-choco install -y P4Merge
-choco install -y pidgin
-#choco install -y pip
-choco install -y kitty
-choco install -y putty
-choco install -y putty.portable
-choco install -y rdcman
-choco install -y redis-64
-choco install -y ruby
-choco install -y screenhero
-choco install -y sharpkeys
-choco install -y slack
-choco install -y sysinternals
-choco install -y terminals
-#choco install -y UrlRewrite # installed later on by install-development-iis.ps1 in backend-scripts, fails if IIS not installed
-choco install -y vlc
