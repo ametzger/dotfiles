e5a3fab4ae73ca2a03783269e59e03b5663c293c
Alex Metzger <asm@asm.io>
2017-10-30 13:35:04 -0400

    Use ripgrep for projectile indexing

--- emacs/personal/personal.el
+++ emacs/personal/personal.el
@@ -174,3 +174,28 @@
 ;; auto-switch to new panes
 (global-set-key "\C-x2" (lambda () (interactive)(split-window-vertically) (other-window 1)))
 (global-set-key "\C-x3" (lambda () (interactive)(split-window-horizontally) (other-window 1)))
+
+;; use RipGrep w prelude
+(setq projectile-enable-caching t)
+
+;;; Default rg arguments
+  ;; https://github.com/BurntSushi/ripgrep
+  (when (executable-find "rg")
+    (progn
+      (defconst modi/rg-arguments
+        `("--line-number"                     ; line numbers
+          "--smart-case"
+          "--follow"                          ; follow symlinks
+          "--mmap")                           ; apply memory map optimization when possible
+        "Default rg arguments used in the functions in `projectile' package.")
+
+      (defun modi/advice-projectile-use-rg ()
+        "Always use `rg' for getting a list of all files in the project."
+        (mapconcat 'identity
+                   (append '("\\rg") ; used unaliased version of `rg': \rg
+                           modi/rg-arguments
+                           '("--null" ; output null separated results,
+                             "--files")) ; get file names matching the regex '' (all files)
+                   " "))
+
+      (advice-add 'projectile-get-ext-command :override #'modi/advice-projectile-use-rg)))
