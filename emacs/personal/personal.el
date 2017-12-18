35ad935f78c759c1ad95baf6b23bb450ec5e953a
Alex Metzger <asm@asm.io>
2017-12-18 11:59:47 -0500

    better helm-swoop behavior

--- emacs/personal/personal.el
+++ emacs/personal/personal.el
@@ -242,7 +242,7 @@
 (setq helm-multi-swoop-edit-save t)
 
 ;; If this value is t, split window inside the current window
-(setq helm-swoop-split-with-multiple-windows t)
+(setq helm-swoop-split-with-multiple-windows nil)
 
 ;; Split direcion. 'split-window-vertically or 'split-window-horizontally
 (setq helm-swoop-split-direction 'split-window-horizontally)
