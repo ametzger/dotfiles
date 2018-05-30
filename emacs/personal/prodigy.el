9d2f357b2f7c80ad3e19bf0043665abcba204bd8
Alex Metzger <asm@asm.io>
2018-05-30 19:07:09 -0400

    emacs: setup prodigy to auto-login to ein

--- emacs/personal/prodigy.el
+++ emacs/personal/prodigy.el
@@ -3,7 +3,7 @@
 (prelude-require-package 'prodigy)
 
 (prodigy-define-service
-  :name "Django Runserver"
+  :name "runserver"
   :command "pipenv"
   :args '("run" "python" "manage.py" "runserver")
   :cwd "~/proj/jellyfish"
@@ -16,7 +16,7 @@
   :url "http://localhost:8000")
 
 (prodigy-define-service
-  :name "Django Celery Worker"
+  :name "celery"
   :command "pipenv"
   :args '("run" "python" "manage.py" "celery")
   :cwd "~/proj/jellyfish"
@@ -29,5 +29,32 @@
                  (cond ((s-matches? ": INFO/MainProcess] celery@[a-z-_]+ ready." output)
                         (prodigy-set-status service 'ready))
                        ((s-matches? "Reloading celery worker..." output)
+                        (prodigy-set-status service 'stopping)))))
+  :kill-process-buffer-on-stop nil)
+
+;; (let (token (cdr ein-token-matches))
+;;   ;; If we see the notebook's token, set up ein
+;;   (ein:notebooklist-login "8888" token)
+;;   (prodigy-set-status service 'ready))
+
+(prodigy-define-service
+  :name "jupyter notebook"
+  :command "pipenv"
+  :args '("run" "python" "manage.py" "shell_plus" "--notebook" "--no-browser")
+  :cwd "~/proj/jellyfish"
+  :path '("~/proj/jellyfish")
+  :tags '(work python)
+  :stop-signal 'sigkill
+  :on-output (lambda (&rest args)
+               (let* ((output (plist-get args :output))
+                      (service (plist-get args :service))
+                      (jupyter-matches (s-match "\\(http://localhost:[0-9]\\{4\\}\\)/\\?token=\\([A-Za-z0-9]+\\)" output)))
+                 (cond (jupyter-matches (let ((jupyter-port (cadr jupyter-matches))
+                                              (jupyter-token (caddr jupyter-matches)))
+                                          (prodigy-set-status service 'ready)
+                                          (ein:notebooklist-login jupyter-port jupyter-token)))
+                       ((s-matches? "The Jupyter Notebook is running at:" output)
+                        (prodigy-set-status service 'ready))
+                       ((s-matches? "received signal [0-9]+, stopping" output)
                         (prodigy-set-status service 'stopping)))))
   :kill-process-buffer-on-stop nil)
