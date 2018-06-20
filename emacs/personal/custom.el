bac227dad56fbf3a018f977fc09342db34b9ffde
Alex Metzger <asm@asm.io>
2018-06-20 11:38:05 -0400

    emacs: add jinja2-mode

--- emacs/personal/custom.el
+++ emacs/personal/custom.el
@@ -12,16 +12,10 @@
    "/Users/asm/proj/vendor/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe")
  '(package-selected-packages
    (quote
-    (prodigy org-journal ein helm-rg projectile-ripgrep ripgrep key-chord elisp-slime-nav rainbow-delimiters smart-mode-line-powerline-theme spaceline-all-the-icons spaceline minimap yafolding use-package format-sql djangonaut linum-off multiple-cursors pipenv ob-ipython rainbow-mode dockerfile-mode vagrant-tramp vagrant nginx-mode ssh-config-mode yaml-mode pyenv-mode restclient dash-at-point cask-mode helm-swoop yasnippet-snippets yasnippet company-anaconda toml-mode rust-mode nord-theme dracula-theme web-mode scss-mode anaconda-mode js2-mode neotree web-beautify terraform-mode go-mode clojure-mode helm-ag helm-descbinds helm-projectile helm elixir-mode powerline markdown-mode inf-ruby csv-mode company gitconfig visual-regexp visual-regexp-steroids hlinum json-mode fish-mode haskell-mode ag racket-mode csharp-mode zop-to-char zenburn-theme volatile-highlights vkill undo-tree solarized-theme smartrep smartparens smart-mode-line projectile ov operate-on-number move-text magit guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell easy-kill discover-my-major diminish diff-hl browse-kill-ring beacon anzu ace-window)))
+    (jinja2-mode prodigy org-journal ein helm-rg projectile-ripgrep ripgrep key-chord elisp-slime-nav rainbow-delimiters smart-mode-line-powerline-theme spaceline-all-the-icons spaceline minimap yafolding use-package format-sql djangonaut linum-off multiple-cursors pipenv ob-ipython rainbow-mode dockerfile-mode vagrant-tramp vagrant nginx-mode ssh-config-mode yaml-mode pyenv-mode restclient dash-at-point cask-mode helm-swoop yasnippet-snippets yasnippet company-anaconda toml-mode rust-mode nord-theme dracula-theme web-mode scss-mode anaconda-mode js2-mode neotree web-beautify terraform-mode go-mode clojure-mode helm-ag helm-descbinds helm-projectile helm elixir-mode powerline markdown-mode inf-ruby csv-mode company gitconfig visual-regexp visual-regexp-steroids hlinum json-mode fish-mode haskell-mode ag racket-mode csharp-mode zop-to-char zenburn-theme volatile-highlights vkill undo-tree solarized-theme smartrep smartparens smart-mode-line projectile ov operate-on-number move-text magit guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell easy-kill discover-my-major diminish diff-hl browse-kill-ring beacon anzu ace-window)))
  '(safe-local-variable-values
    (quote
     ((pony-settings make-pony-project :python "/Users/asm/.pyenv/shims/python" :pythonpath "/Users/asm/proj/jellyfish" :settings "jellyfish.settings.dev")
      (pony-settings make-pony-project :python "/Users/asm/.pyenv/shims/python" :settings "jellyfish.settings.dev")
      (pony-settings
       (make-pony-project :python "/Users/asm/.pyenv/shims/python" :settings "jellyfish.settings.dev"))))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
