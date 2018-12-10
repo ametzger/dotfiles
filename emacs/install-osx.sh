94cd72e36e78c6251485f471090426c20932ff24
Alex Metzger <asm@asm.io>
2018-12-10 13:45:15 -0500

    emacs: reorganize config

--- emacs/install-osx.sh
+++
@@ -1,9 +0,0 @@
-  #!/usr/bin/env bash
-
-brew install --with-cocoa emacs
-pushd ~/proj
-git clone https://github.com/bbatsov/prelude.git
-ln -s ~/proj/prelude ~/.emacs.d
-pushd ~/proj/prelude
-rm -rf personal
-ln -s ~/proj/config/emacs/personal personal
