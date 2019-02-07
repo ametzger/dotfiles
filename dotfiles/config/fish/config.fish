6e5acac9e7e17ebcaf2d9e7970900e5ed743ef87
Alex Metzger <asm@asm.io>
2019-02-07 13:46:40 -0800

    fish: add more cfg

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -10,6 +10,15 @@ if test -d $HOME/bin
     set -g fish_user_paths "$HOME/bin" $fish_user_paths
 end
 
+# add ~/.local/bin to PATH if it exists
+if test -d $HOME/.local/bin
+    #echo "Adding ~/bin to PATH"
+    set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
+end
+
+set -Ux EDITOR nvim
+set -gx default_user "asm"
+
 alias e "emacsclient -nac"
 alias mvim "emacsclient -nac"
 alias subl "emacsclient -nac"
