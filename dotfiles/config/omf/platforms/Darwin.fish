e426929eb7d5ad26f0ecef2c12aa93cc0bf21170
Alex Metzger <asm@asm.io>
2019-02-25 17:04:31 -0500

    fish: add brew libs to compile paths

--- dotfiles/config/omf/platforms/Darwin.fish
+++ dotfiles/config/omf/platforms/Darwin.fish
@@ -11,6 +11,6 @@ set -gx HOMEBREW_INSTALL_CLEANUP 1
 
 # Link against brew openssl, zlib (Newer Python versions seem to require
 # this)
-set -gx LDFLAGS "-L/usr/local/opt/openssl/lib -L/usr/local/opt/zlib/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/openssl/include -I/usr/local/opt/zlib/include"
-set -gx PKG_CONFIG_PATH "/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/zlib/lib/pkgconfig"
+set -gx LDFLAGS "-L/usr/local/lib -L/usr/local/opt/openssl/lib -L/usr/local/opt/zlib/lib"
+set -gx CPPFLAGS "-I/usr/local/include -I/usr/local/opt/openssl/include -I/usr/local/opt/zlib/include"
+set -gx PKG_CONFIG_PATH "/usr/local/lib/pkgconfig:/usr/local/opt/openssl/lib/pkgconfig:/usr/local/opt/zlib/lib/pkgconfig"
