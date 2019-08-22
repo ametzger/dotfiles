4dfabfaf14e17f47649610454e251dc39c12df22
Alex Metzger <asm@asm.io>
2019-08-21 22:02:17 -0400

    more linux ansible

--- roles/emacs/files/doom/config.el
+++ roles/emacs/files/doom/config.el
@@ -1,9 +1,9 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-(let ((font-size (if (eq system-type 'darwin)
-                     17
-                   13)))
-  (setq doom-font (format "Operator Mono Medium %d" font-size)))
+; (let ((font-size (if (eq system-type 'darwin)
+;                      17
+;                    13)))
+;   (setq doom-font (format "Operator Mono Medium %d" font-size)))
 
 (custom-set-faces
  '(font-lock-comment-face ((t (:foreground "#6d7a96" :slant italic))))
