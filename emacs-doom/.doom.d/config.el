bc298ed23fb21b8b6e06a0ae1d5caba9e28160ee
Alex Metzger <asm@asm.io>
2020-10-20 15:54:10 -0400

    doom emacs: flake8

--- emacs-doom/.doom.d/config.el
+++ emacs-doom/.doom.d/config.el
@@ -6,6 +6,10 @@
   (setq doom-font (format "Operator Mono Medium %d" font-size)))
 
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(font-lock-comment-face ((t (:foreground "#6d7a96" :slant italic))))
  '(font-lock-doc-face ((t (:foreground "#6d7a96" :slant italic))))
  '(font-lock-keyword-face ((t (:foreground "#81A1C1" :slant italic)))))
@@ -14,3 +18,10 @@
 
 (setq default-directory "~/"
       avy-all-windows t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((flycheck-flake8rc . "/Users/asm/proj/jellyfish/.flake8"))))
