7712e05c8d4ba56ed6727d3deef6595701bab607
Alex Metzger <asm@asm.io>
2023-01-11 17:20:52 -0500

    nix: moar packages

--- etc/nixpkgs/modules/packages.nix
+++ etc/nixpkgs/modules/packages.nix
@@ -10,8 +10,48 @@
 
   home.packages = with pkgs;
     [
-      neovim
+      awscli
+      bat
+      black
+      coreutils
+      curl
+      direnv
+      elixir
+      exa
       fd
+      fish
+      fzf
+      gnused
+      gnupg
+      go
+      httpie
+      hyperfine
+      jq
+      kakoune
+      xmlsec
+      mtr
+      neovim
+      nmap
+      nodejs
+      openblas
+      openssl
+      pgbouncer
+      postgresql
+      python310Full
+      rabbitmq-server
+      reattach-to-user-namespace
+      redis
+      ripgrep
+      ruby
+      starship
+      tmuxinator
+      tokei
+      tree
+      wget
+      wrk
+      zlib
+      zoxide
+      zsh
     ];
 
   home.file.".config/nvim/init.vim".source = ../../nvim.vim;
