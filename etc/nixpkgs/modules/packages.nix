731f1b785a08397d802e272eeb139cd28fb6fe9d
Alex Metzger <asm@asm.io>
2023-01-11 18:10:11 -0500

    nix: move more config files into home-manager

--- etc/nixpkgs/modules/packages.nix
+++ etc/nixpkgs/modules/packages.nix
@@ -54,5 +54,28 @@
       zsh
     ];
 
+  home.file.".asdfrc".source = ../../asdfrc;
+  home.file.".bashrc".source = ../../bashrc;
+  home.file.".config/alacritty/alacritty.yml".source = ../../alacritty.yml;
+  home.file.".config/black".source = ../../black;
+  home.file.".config/fish/config.fish".source = ../../config.fish;
+  # home.file.".config/fish/functions".source = ../../fish-functions;
+  home.file.".config/flake8".source = ../../flake8;
+  home.file.".config/kitty/kitty.conf".source = ../../kitty.conf;
   home.file.".config/nvim/init.vim".source = ../../nvim.vim;
+  home.file.".config/zellij/config.kdl".source = ../../zellij.kdl;
+  home.file.".direnvrc".source = ../../direnvrc;
+  home.file.".editorconfig".source = ../../editorconfig;
+  home.file.".gemrc".source = ../../gemrc;
+  home.file.".hyper.js".source = ../../hyper.js;
+  home.file.".irbrc".source = ../../irbrc;
+  home.file.".psqlrc".source = ../../psqlrc;
+  home.file.".pylintrc".source = ../../pylintrc;
+  home.file.".ripgreprc".source = ../../ripgreprc;
+  home.file.".spacemacs".source = ../../spacemacs;
+  home.file.".ssh/config".source = ../../sshconfig;
+  home.file.".tmux.conf".source = ../../tmux.conf;
+  home.file.".tmuxinator.yml".source = ../../tmuxinator.yml;
+  home.file.".tool-versions".source = ../../tool-versions;
+
 }
