1e897678a8996e8a85ef4355e763bc21dd1f0913
Alex Metzger <asm@asm.io>
2023-03-24 21:53:35 -0400

    wip: emacs in nix

--- etc/home-manager/flake.nix
+++ etc/home-manager/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Home Manager configuration";
+  description = "asm home-manager config";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
@@ -12,9 +12,10 @@
       url = "github:nix-community/emacs-overlay/702b1724ead7b6eec28bfc5e1404c26a57a3b248";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { self, nixpkgs, home-manager, devenv, emacs-overlay, ... } @ inputs: let
+  outputs = { self, nixpkgs, home-manager, devenv, emacs-overlay, nur, ... } @ inputs: let
     inherit (nixpkgs.lib) optionalAttrs singleton optionals;
 
     supportedSystems = ["x86_64-darwin" "aarch64-darwin"];
@@ -24,9 +25,7 @@
       singleton (
         # Sub in x86 version of packages that don't build on Apple Silicon yet
         final: prev: (optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-          inherit
-            (final.pkgs-x86)
-            ;
+          inherit (final.pkgs-x86);
         })
       )
       ++ [emacs-overlay.overlay];
@@ -41,13 +40,20 @@
           };
         }
     );
-  in {
-    defaultPackage.aarch64-darwin = self.homeConfigurations.asm.activationPackage;
-    defaultPackage.x86_64-darwin = self.homeConfigurations."asm@asm-mbp-16".activationPackage;
 
+    nurModules = import nur {
+      nurpkgs = legacyPackages.x86_64-darwin;
+      pkgs = legacyPackages.x86_64-darwin;
+    };
+ in {
     homeConfigurations."asm@asm-mbp-16" = home-manager.lib.homeManagerConfiguration {
       pkgs = legacyPackages."x86_64-darwin";
-      modules = [ ./home.nix ];
+      modules = [
+        ./home.nix
+        {
+          imports = [ nurModules.repos.rycee.hmModules.emacs-init ];
+        }
+      ];
     };
 
     homeConfigurations.asm = home-manager.lib.homeManagerConfiguration {
@@ -62,5 +68,8 @@
         }
       ];
     };
+
+    defaultPackage.aarch64-darwin = self.homeConfigurations.asm.activationPackage;
+    defaultPackage.x86_64-darwin = self.homeConfigurations."asm@asm-mbp-16".activationPackage;
   };
 }
--- etc/home-manager/modules/packages/emacs/default.nix
+++ etc/home-manager/modules/packages/emacs/default.nix
@@ -1,7 +1,7 @@
-{ config, lib, pkgs, ... }:
-{
+{ config, lib, pkgs, ... }: {
   programs.emacs = {
     enable = true;
+
     package = with pkgs; pkgs.emacsPgtk.overrideAttrs (old: {
       patches =
         (old.patches or [])
@@ -23,5 +23,119 @@
           })
         ];
     });
+
+    init = {
+      enable = true;
+      recommendedGcSettings = true;
+
+      prelude = ''
+      ;; Disable startup message.
+      (setq inhibit-startup-message t
+            inhibit-startup-echo-area-message (user-login-name))
+      '';
+
+      usePackage = {
+        abbrev = {
+          enable = true;
+          diminish = [ "abbrev-mode" ];
+          command = [ "abbrev-mode" ];
+        };
+      };
+    };
+
+    extraPackages = (epkgs:
+      (with epkgs; [
+        ace-window
+        all-the-icons
+        all-the-icons-dired
+        anzu
+        avy
+        blacken
+        browse-kill-ring
+        cider
+        clojure-mode
+        company
+        company-go
+        company-jedi
+        company-terraform
+        counsel
+        crux
+        deadgrep
+        direnv
+        discover-my-major
+        dockerfile-mode
+        doom-modeline
+        doom-themes
+        dumb-jump
+        dwim-shell-command
+        easy-kill
+        ein
+        elixir-mode
+        exec-path-from-shell
+        expand-region
+        flycheck-mypy
+        flycheck-rust
+        git-link
+        git-timemachine
+        go-eldoc
+        go-guru
+        go-mode
+        go-projectile
+        gorepl-mode
+        hl-todo
+        iedit
+        imenu-anywhere
+        imenu-list
+        ivy
+        ivy-prescient
+        jedi
+        jinja2-mode
+        js2-mode
+        json-mode
+        just-mode
+        lsp-ivy
+        lsp-mode
+        lsp-pyright
+        lsp-ui
+        magit
+        markdown-mode
+        move-text
+        multiple-cursors
+        neotree
+        nix-mode
+        org
+        org-bullets
+        persp-projectile
+        perspective
+        pipenv
+        prescient
+        projectile
+        pt
+        py-isort
+        pyenv-mode
+        racer
+        rainbow-delimiters
+        reformatter
+        restclient
+        rust-mode
+        s
+        sdlang-mode
+        shackle
+        smartparens
+        swiper
+        terraform-mode
+        toml-mode
+        typescript-mode
+        undo-tree
+        volatile-highlights
+        vterm
+        web-mode
+        wgrep
+        which-key
+        yaml-mode
+        yasnippet
+        yasnippet-snippets
+        zop-to-char
+      ]));
   };
 }
