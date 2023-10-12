74c96b52322df32069f1abca514711923252c3a1
Alex Metzger <asm@asm.io>
2023-10-12 11:53:33 -0400

    nix: more ssh config

--- etc/home-manager/modules/packages/ssh.nix
+++ etc/home-manager/modules/packages/ssh.nix
@@ -18,6 +18,17 @@
             sh -c "aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p'"
           '';
       };
+
+      "util" = {
+        user = "ec2-user";
+        identityFile = "~/.ssh/id_ed25519";
+        checkHostIP = false;
+        extraOptions = {
+          StrictHostKeyChecking = "no";
+          UserKnownHostsFile = "/dev/null";
+        };
+        proxyCommand = "~/bin/util-ssh";
+      };
     };
   };
 }
